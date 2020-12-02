f0f5bf6299b8bfd6a61bd366b8fe18e8af0f7f16
Oleg Kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-12-02 18:17:23 +0300

    part 6

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -64,7 +64,7 @@ class Entity {
    * */
   static remove( id = '', data, callback = f => f ) {
 
-    const modifiedData = Object.assign({ _method: 'DELETE'}, id ,  data );
+    const modifiedData = Object.assign({ _method: 'DELETE', id },  data );
     const options = {
       data: modifiedData ,
       url: this.URL,
